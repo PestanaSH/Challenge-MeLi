042e68aad968d412eaa22dd21af56c590b7680c7
Lucas Ribeiro Pestana <lucaspestana0212@gmail.com>
2022-08-06 18:37:51 -0300

    connect to database

---
+++ db.py
@@ -0,0 +1,41 @@
+import mysql.connector
+
+
+def connection():
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Abc@1234",
+        database="driver"
+    )
+    print(mydb)
+    return mydb
+
+
+# Função para inserir os dados na base de dados
+def insertData():
+    mydb = connection()
+    mycursor = mydb.cursor()
+
+    sql = "INSERT INTO files (id, fileName, extension, fileOwner, lastModify, visibility) " \
+          "VALUES (%s, %s, %s, %s, %s, %s)"
+    val = (2, "testePython", ".pdf", "lucas.pestana","2022-04-01",  "publico")
+    mycursor.execute(sql, val)
+
+    mydb.commit()
+    print(mycursor.rowcount, "record inserted.")
+
+def selectAll():
+    mydb = connection()
+
+    mycursor = mydb.cursor()
+
+    mycursor.execute("SELECT * FROM files")
+
+    myresult = mycursor.fetchall()
+
+    for x in myresult:
+        print(x)
+
+insertData()
+selectAll()
--- main.py
+++ main.py
@@ -1,62 +1,15 @@
 from __future__ import print_function
 
-import os.path
-
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
 from APIDrive import api
+from db import connection
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/drive']
 
-
-# def main():
-#     """Shows basic usage of the Drive v3 API.
-#     Prints the names and ids of the first 10 files the user has access to.
-#     """
-#     creds = None
-#     # The file token.json stores the user's access and refresh tokens, and is
-#     # created automatically when the authorization flow completes for the first
-#     # time.
-#     if os.path.exists('token.json'):
-#         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-#     # If there are no (valid) credentials available, let the user log in.
-#     if not creds or not creds.valid:
-#         if creds and creds.expired and creds.refresh_token:
-#             creds.refresh(Request())
-#         else:
-#             flow = InstalledAppFlow.from_client_secrets_file(
-#                 'client_secret.json', SCOPES)
-#             creds = flow.run_local_server(port=0)
-#         # Save the credentials for the next run
-#         with open('token.json', 'w') as token:
-#             token.write(creds.to_json())
-#
-#     try:
-#         service = build('drive', 'v3', credentials=creds)
-#
-#         # Call the Drive v3 API
-#         results = service.files().list(
-#             pageSize=10, fields="nextPageToken, files(id, name)").execute()
-#         items = results.get('files', [])
-#
-#         if not items:
-#             print('No files found.')
-#             return
-#         print('Files:')
-#         for item in items:
-#             print(u'{0} ({1})'.format(item['name'], item['id']))
-#     except HttpError as error:
-#         # TODO(developer) - Handle errors from drive API.
-#         print(f'An error occurred: {error}')
 def main():
     print('Start Application')
     service = api()
-    # Call the Drive v3 API
-
+    connection()
     try:
         results = service.files().list(
             pageSize=1000, fields="nextPageToken, files(*)").execute()
@@ -67,26 +20,13 @@ def main():
             return
         print('File:')
         for item in items:
-            # print(u'{0} ({1})'.format(item['name'], item['id']))
-            # print(f"Id: {item['id']}")
-            # print(f"Name: {item['name']}")
-            # print(f"Owner: {item['owners'][0]['emailAddress']}")
-            # print(f"shared: {item['shared']}")
-            # print(f"modifiedTime: {item['modifiedTime']}")
-            # print(f"mimeType: {item['mimeType']}")
-            file = [item['id'], item['name'], item['owners'][0]['emailAddress'], item['shared'], item['modifiedTime'], item['mimeType']]
+            file = [item['id'], item['name'], item['owners'][0]['emailAddress'], item['shared'], item['modifiedTime'],
+                    item['mimeType']]
             print(file)
 
-    except Exception:
-        print(f'Error')
 
-    # if not items:
-    #     print('No files found.')
-    #     return
-    # print('Files:')
-    # for item in items:
-    #     print(u'{0} ({1})'.format(item['name'], item['id']))
-
+    except Exception as error:
+        print(f'Error {error}')
 
 if __name__ == '__main__':
     main()
